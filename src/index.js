a1896ebc78a5cb572fcc89f830cae49694b1a9c8
Brandon <brandon@slantedshots.com>
2022-10-09 12:24:25 -0500

    k

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchWithTimeout, fetchBooks, fetchMovies } from './services';
+import { fetchWithTimeout, fetchBooks, fetchMovies, asyncFetchBooks, asyncFetchMovies } from './services';
 const movies = require('./data/movies.json');
 
 const getBooksAndMovies = () => {
@@ -27,3 +27,34 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
     console.log('getBooksOrMoviesPromise', results);
 });
+
+//async functions
+
+async function getBooksAndMoviesAsync() {
+    try {
+        const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
+        return { books, movies};
+    } catch (error) {
+        console.error('Error Fetching books and movies', error);   
+    }
+}
+
+async function getBooksOrMoviesAsync() {
+    try {
+       const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+       return { values };
+      }
+    catch (error) {
+        console.error("Error waiting for the promise race", error);
+}};
+
+getBooksAndMoviesAsync()
+    .then(results => console.log("movies and books", {
+        movies: results.movies,
+        books: results.books
+      }));
+
+getBooksOrMoviesAsync()
+    .then(results => console.log("movies OR books", {
+        results
+      }));
