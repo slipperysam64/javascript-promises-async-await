93db4657d03ef68735151d36dbc31c731b0c62d8
Brandon <brandon@slantedshots.com>
2022-10-09 12:39:54 -0500

    cf

--- src/index.js
+++ src/index.js
@@ -35,26 +35,28 @@ async function getBooksAndMoviesAsync() {
         const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
         return { books, movies};
     } catch (error) {
-        console.error('Error Fetching books and movies', error);   
+        console.log('Error fetching books and movies', error);   
     }
 }
 
 async function getBooksOrMoviesAsync() {
     try {
        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-       return { values };
+       return values;
       }
     catch (error) {
         console.error("Error waiting for the promise race", error);
 }};
 
-getBooksAndMoviesAsync()
-    .then(results => console.log("movies and books", {
+getBooksAndMoviesAsync().then(results => {
+    console.log("movies AND books", {
         movies: results.movies,
-        books: results.books
-      }));
+        books: results.books,
+      });
+    });
 
-getBooksOrMoviesAsync()
-    .then(results => console.log("movies OR books", {
-        results
-      }));
+getBooksOrMoviesAsync().then(results => {
+    console.log('movies OR books', {
+        results,
+    });
+});
