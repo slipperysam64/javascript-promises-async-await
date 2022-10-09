d5bcbe02532f1ac11172a9d27245f93b81bbbe38
Brandon <brandon@slantedshots.com>
2022-10-09 11:31:33 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import { fetchWithTimeout } from './services';
+import { fetchWithTimeout, fetchBooks, fetchMovies } from './services';
 const movies = require('./data/movies.json');
 
-export function fetchMovies() {
-    const resolveFunction = () =>movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+const getBooksAndMovies = () => {
+    return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies]) => ({
+        books, movies
+    }))
+    .catch(error => console.log("eror Fetching books and movies", error))
 }
 
-const moviePromise = fetchMovies();
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksandMoviesPromise', results);
+});
 
-moviePromise.then(results => {
-    console.log(results);
-})
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+    .catch(error => {
+        console.log('Error waiting for the promise race', error);
+    });
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => {
+    console.log('getBooksOrMoviesPromise', results);
+});
